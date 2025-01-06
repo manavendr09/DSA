ec0e5742b4bd94cc4665add0210481759952fdda
manavendr09 <manavendra.23b1531211@abes.ac.in>
2025-01-06 23:55:24 +0530

    leetcode problem 485

---
+++ maxConcecutiveOnes.java
@@ -0,0 +1,19 @@
+package DSA;
+
+class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int max=0,c=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==1){
+                c++;
+                if(c>=max){
+                    max=c;
+                }
+            }
+            else{
+                c=0;
+            }
+        } 
+        return max;
+    }
+}
