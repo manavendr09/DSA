027aa812b6b78614fe626854f2894b0cbfb2732b
manavendr09 <manavendra.23b1531211@abes.ac.in>
2025-01-09 19:19:12 +0530

    leetcode problem 169

--- majorityElement.java
+++ majorityElement.java
@@ -1,23 +1,27 @@
 package DSA;
 
 class Solution {
-    public int majorityElement(int[] nums) {
-        int n=nums.length/2;
-        int count=0;
-        int cand=0;
-        for (int i=0;i<nums.length;i++){
-              if(count==0){
-                cand=nums[i];
+  public int majorityElement(int[] nums) {
+      int n=nums.length/2;
+      int count=0;
+      int cand=0;
+      for (int i=0;i<nums.length;i++){
+            if(count==0){
+              count=1;
+              cand=nums[i];
+            }
+            else if(cand==nums[i])  count++;
+            else count--;
+      }
+        int  count1=0;
+            for(int j=0;j<nums.length;j++){
+              if(nums[j]==cand){
+                  count1++;
               }
-                count += (cand==nums[i])?1:-1;
-              
-        }
-              count=0;
-              for(int j=0;j<nums.length;j++){
-                if(nums[j]==cand){
-                    count++;
-                }
-    }
-    return (count>n)?cand:-1;
+  }
+  return (count1>n)?cand:-1;
 }
 }
+
+
+///// MOORE'S VOTING ALGORITHM
