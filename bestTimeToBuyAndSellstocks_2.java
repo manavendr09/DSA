b4709916d77afcffc5ab4d786a6fcbc87a0eb5e5
manavendr09 <manavendra.23b1531211@abes.ac.in>
2025-01-11 23:01:13 +0530

    leetcode problem 122

---
+++ bestTimeToBuyAndSellstocks_2.java
@@ -0,0 +1,14 @@
+package DSA;
+
+class Solution {
+    public int maxProfit(int[] prices) {
+        int tp=0;
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] > prices[i - 1]) {
+             tp += (prices[i] - prices[i - 1]);
+            }
+        }
+        
+        return tp;
+    }
+}
