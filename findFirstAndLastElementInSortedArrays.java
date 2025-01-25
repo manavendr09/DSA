9fa6cd0e444618992699878d0c2df5f7a29c3f86
manavendr09 <manavendra.23b1531211@abes.ac.in>
2025-01-25 23:54:26 +0530

    leetcode 34

---
+++ findFirstAndLastElementInSortedArrays.java
@@ -0,0 +1,27 @@
+package DSA;
+
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        int[] ans=new int[2];
+        ans[0]=-1;
+        ans[1]=-1;
+     
+        int k=nums.length-1;
+        for(int i=0;i<nums.length;i++){
+              if(nums[i]==target){
+              final int j=i;
+                          ans[0]=j;
+
+              }
+              if(nums[k]==target){
+                final int a=k;
+             ans[1]=k;
+
+              }
+              k--;
+
+            }
+            Arrays.sort(ans);
+            return ans;
+        }
+    }
