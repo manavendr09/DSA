f601d8e26241e54ab54b8c52f9b0a6e44be59c1a
Manavendra Yadav <manavendra.23b1531211@abes.ac.in>
2026-07-04 16:34:17 +0530

    feat: update LeetCode 1365. How Many Numbers Are Smaller Than the Current Number solution (java)

--- problems/how-many-numbers-are-smaller-than-the-current-number/solution.java
+++ problems/how-many-numbers-are-smaller-than-the-current-number/solution.java
@@ -1,16 +1,32 @@
 class Solution {
     public int[] smallerNumbersThanCurrent(int[] nums) {
-        int n=nums.length;
-        int ans[]=new int[n];
-        for(int i=0;i<n;i++){
-            int c=0;
-            for(int j=0;j<n;j++){
-                if(nums[i]>nums[j]){
-                    c++;
-                }
-            }
-            ans[i]=c;
+        // int n=nums.length;
+        // int ans[]=new int[n];
+        // for(int i=0;i<n;i++){
+        //     int c=0;
+        //     for(int j=0;j<n;j++){
+        //         if(nums[i]>nums[j]){
+        //             c++;
+        //         }
+        //     }
+        //     ans[i]=c;
+        // }
+        // return ans;
+        int[] freq=new int[101];
+        for(int num:nums) {
+            freq[num]++;
         }
+        for(int i=1;i<=100;i++) {
+            freq[i]+=freq[i-1];
+        }
+        int[] ans = new int[nums.length];
+        for (int i=0;i<nums.length;i++) {
+            if(nums[i]==0)
+                ans[i]=0;
+            else
+                ans[i]=freq[nums[i]-1];
+        }
+
         return ans;
     }
 }
