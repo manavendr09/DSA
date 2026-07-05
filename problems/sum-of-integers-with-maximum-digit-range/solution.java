e41dab01c074494a462198baf88c2b558093692a
Manavendra Yadav <manavendra.23b1531211@abes.ac.in>
2026-07-05 15:01:44 +0530

    feat: update LeetCode 3982. Sum of Integers with Maximum Digit Range solution (java)

--- problems/sum-of-integers-with-maximum-digit-range/solution.java
+++ problems/sum-of-integers-with-maximum-digit-range/solution.java
@@ -1,26 +1,59 @@
 class Solution {
     public int maxDigitRange(int[] nums) {
-        int dr[]=new int[nums.length];
+      int dr[]=new int[nums.length];
         int c=0;
         for(int i=0;i<nums.length;i++){
-            int a=dif(nums[i]);
-            dr[i]=a;
+            int num = nums[i];
+            int max = Integer.MIN_VALUE;
+            int min = Integer.MAX_VALUE;
+            if (num == 0) {
+                max = 0;
+                min = 0;
+            } 
+            while (num > 0) {
+                max = Math.max(max, num % 10);
+                min = Math.min(min, num % 10);
+                num /= 10;
+            }
+            int a= max - min;
+           // int a=dif(nums[i]);
+           dr[i]=a;
             if(a>c) c=a;
-        }int s=0;
-        for(int i=0;i<nums.length;i++){
-          if(dr[i]==c) s+=nums[i];
         }
+        int s=0;
+        // for(int i=0;i<nums.length;i++){
+        //   if(dr[i]==c) s+=nums[i];
+        // }
+         for(int i=0;i<nums.length;i++){
+        //      int num = nums[i];
+        //     int max = Integer.MIN_VALUE;
+        //     int min = Integer.MAX_VALUE;
+        //     if (num == 0) {
+        //         max = 0;
+        //         min = 0;
+        //     } 
+        //     while (num > 0) {
+        //         max = Math.max(max, num % 10);
+        //         min = Math.min(min, num % 10);
+        //         num /= 10;
+        //     }
+        //     int a= max - min;
+           // int a=dif(nums[i]);
+            if(dr[i]==c){
+                s+=nums[i];
+            }
+         }
         return s;
     }
-    public int dif(int n){
-        int max=0,min=Integer.MAX_VALUE;
-       while(n>0){
-        int a=n%10;
-        if(max<a) max=a;
-        if(min>a) min=a;
-        n=n/10;
+    // public int dif(int n){
+    //     int max=0,min=Integer.MAX_VALUE;
+    //    while(n>0){
+    //     int a=n%10;
+    //     if(max<a) max=a;
+    //     if(min>a) min=a;
+    //     n=n/10;
         
-       }
-       return max-min;
-    }
+    //    }
+    //    return max-min;
+    //}
 }
