dc750c3bec67c2ba239e220e017f4869c23d2a17
Manavendra Yadav <manavendra.23b1531211@abes.ac.in>
2026-07-08 01:09:12 +0530

    feat: update LeetCode 3754. Concatenate Non-Zero Digits and Multiply by Sum I solution (java)

--- problems/concatenate-non-zero-digits-and-multiply-by-sum-i/solution.java
+++ problems/concatenate-non-zero-digits-and-multiply-by-sum-i/solution.java
@@ -1,11 +1,33 @@
 class Solution {
     public long sumAndMultiply(int n) {
-        long x = 0, s = 0;
-        for (char c : String.valueOf(n).toCharArray())
-            if (c != '0') {
-                x = x * 10 + c - '0';
-                s += c - '0';
-            }
-        return x * s;
+        int x=0;
+        int sum=0;
+        int i=1;
+        while(n>0){
+            int m=n % 10;
+            n /=10;
+            if(m == 0) continue;
+            sum += m;
+            x += (m * i);
+            i *=10;
+        }
+        return (long) x * sum;
+
+        // if(n==0) return 0;
+        // StringBuilder st=new StringBuilder();
+        // int s=0;
+        // while(n>0){
+        //     int a=n%10;
+        //     if(a!=0){
+        //         s+=a;
+        //     st.append(a);
+        //     }
+        //     n=n/10;
+        // }
+        // st.reverse();
+        // String s1=st.toString();
+        // int x=Integer.parseInt(s1);
+        // long l=(long)x*(long)s;
+        // return l;
     }
 }
